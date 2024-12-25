c1059ca6bdccee4c1f27f0a73288834db2c0fbaf
Misael-GC <misael.gomez1623@gmail.com>
2024-12-25 11:33:15 -0600

    Configuración de Plantillas para Vistas Reutilizables en Laravel

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
 /*rutas url, que te mandan a los diferentes sitios */
 
@@ -16,10 +16,10 @@ Route::get('blog', function () {
     ];
 
     return view('blog', ['posts' => $posts]);
-});
+})->name('blog');
 
 Route::get('blog/{slug}', function ($slug) {
     //consulta a la BD
     $post = $slug;
     return view('post', ['post' => $post]);
-});
+})->name('post');
